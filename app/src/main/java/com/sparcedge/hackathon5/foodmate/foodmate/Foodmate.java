552fac1406adb50f1fd6c187ce820a059a918e92
Noah Herron <jayssbronze@gmail.com>
2015-08-28 21:04:24 -0400

    first commit

---
+++ app/src/main/java/com/sparcedge/hackathon5/foodmate/foodmate/Foodmate.java
@@ -0,0 +1,50 @@
+package com.sparcedge.hackathon5.foodmate.foodmate;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+/*
+    This is the class that controls the login for the app
+ */
+
+public class Foodmate extends AppCompatActivity implements OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foodmate);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_foodmate, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //get the user name and pass it through to the GroceryList View
+    @Override
+    public void onClick(View v) {
+
+    }
+}
